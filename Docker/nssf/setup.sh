53df9998293f098aacc448b1e7cf7dd548a31efe
przemo1232 <79700515+przemo1232@users.noreply.github.com>
2023-02-11 16:23:59 +0100

    update nssf script

--- Docker/nssf/setup.sh
+++ Docker/nssf/setup.sh
@@ -1,11 +1,60 @@
 #!/bin/sh
-if [ -n "$SBI_ADDR" ]; then
-    sed -z -i "s/\(nssf:\n[^#]*sbi:\n *[-]\{0,1\} addr:\) [0-9.]\{1,\}/\1 $SBI_ADDR/" /etc/open5gs/nssf.yaml
+CONFIG=/etc/open5gs/nssf.yml
+if ! [ -f "$CONFIG-original" ]; then
+    mv $CONFIG $CONFIG-original
 fi
-if [ -n "$NSI_ADDR" ]; then
-    sed -z -i "s/\(nssf:\n[^#]*nsi:\n *[-]\{0,1\} addr:\) [0-9.]\{1,\}/\1 $NSI_ADDR/" /etc/open5gs/nssf.yaml
+if [ -z "$NSSF_ADDR" ]; then
+    NSSF_ADDR=127.0.0.14
 fi
-if [ -n "$SCP_ADDR" ]; then
-    sed -z -i "s/\(scp:\n[^#]*sbi:\n *[-]\{0,1\} addr:\) [0-9.]\{1,\}/\1 $SCP_ADDR/" /etc/open5gs/nssf.yaml
+if [ -z "$NSSF_PORT" ]; then
+    NSSF_PORT=7777
 fi
+if [ -z "$NSI_ADDR" ]; then
+    NSI_ADDR=127.0.0.10
+fi
+if [ -z "$NSI_PORT" ]; then
+    NSI_PORT=7777
+fi
+if [ -z "$SCP_ADDR" ]; then
+    SCP_ADDR=127.0.1.10
+fi
+if [ -z "$SCP_PORT" ]; then
+    SCP_PORT=7777
+fi
+printf "logger:
+    file: @localstatedir@/log/open5gs/nssf.log
+
+tls:
+    enabled: no
+    server:
+      cacert: @sysconfdir@/open5gs/tls/ca.crt
+      key: @sysconfdir@/open5gs/tls/nssf.key
+      cert: @sysconfdir@/open5gs/tls/nssf.crt
+    client:
+      cacert: @sysconfdir@/open5gs/tls/ca.crt
+      key: @sysconfdir@/open5gs/tls/nssf.key
+      cert: @sysconfdir@/open5gs/tls/nssf.crt
+
+nssf:
+    sbi:
+      - addr: $NSSF_ADDR
+        port: $NSSF_PORT
+    nsi:
+      - addr: $NSI_ADDR
+        port: $NSI_PORT
+        s_nssai:
+          sst: 1
+
+scp:
+    sbi:
+      - addr: $SCP_ADDR
+        port: $SCP_PORT
+
+
+parameter:
+
+max:
+
+time:
+" > $CONFIG
 /bin/open5gs-nssfd
