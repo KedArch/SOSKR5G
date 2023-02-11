2075441d7a43b28ee33694c66f2c5778fddd283b
przemo1232 <79700515+przemo1232@users.noreply.github.com>
2023-02-11 16:24:16 +0100

    update ausf script

--- Docker/ausf/setup.sh
+++ Docker/ausf/setup.sh
@@ -1,8 +1,49 @@
 #!/bin/sh
-if [ -n "$SBI_ADDR" ]; then
-    sed -z -i "s/\(ausf:\n[^#]*sbi:\n *[-]\{0,1\} addr:\) [0-9.]\{1,\}/\1 $SBI_ADDR/" /etc/open5gs/ausf.yaml
+CONFIG=/etc/open5gs/ausf.yml
+if ! [ -f "$CONFIG-original" ]; then
+    mv $CONFIG $CONFIG-original
 fi
-if [ -n "$SCP_ADDR" ]; then
-    sed -z -i "s/\(scp:\n[^#]*sbi:\n *[-]\{0,1\} addr:\) [0-9.]\{1,\}/\1 $SCP_ADDR/" /etc/open5gs/ausf.yaml
+if [ -z "$AUSF_ADDR" ]; then
+    AUSF_ADDR=127.0.0.11
 fi
+if [ -z "$AUSF_PORT" ]; then
+    AUSF_PORT=7777
+fi
+if [ -z "$SCP_ADDR" ]; then
+    SCP_ADDR=127.0.1.10
+fi
+if [ -z "$SCP_PORT" ]; then
+    SCP_PORT=7777
+fi
+printf "logger:
+    file: @localstatedir@/log/open5gs/ausf.log
+
+tls:
+    enabled: no
+    server:
+      cacert: @sysconfdir@/open5gs/tls/ca.crt
+      key: @sysconfdir@/open5gs/tls/ausf.key
+      cert: @sysconfdir@/open5gs/tls/ausf.crt
+    client:
+      cacert: @sysconfdir@/open5gs/tls/ca.crt
+      key: @sysconfdir@/open5gs/tls/ausf.key
+      cert: @sysconfdir@/open5gs/tls/ausf.crt
+
+ausf:
+    sbi:
+      - addr: $AUSF_ADDR
+        port: $AUSF_PORT
+
+scp:
+    sbi:
+      - addr: $SCP_ADDR
+        port: $SCP_PORT
+
+
+parameter:
+
+max:
+
+time:
+" > $CONFIG
 /bin/open5gs-ausfd
