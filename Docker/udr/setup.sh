312d1c864b06af8b73d72b70bdbc5bf427abc1ed
przemo1232 <79700515+przemo1232@users.noreply.github.com>
2023-02-11 15:46:59 +0100

    update udr script

--- Docker/udr/setup.sh
+++ Docker/udr/setup.sh
@@ -1,11 +1,56 @@
 #!/bin/sh
-if [ -n "$DB_URI" ]; then
-    sed -i "s/^db_uri: .*/db_uri: $DB_URI" /etc/open5gs/pcf.yaml
+CONFIG=/etc/open5gs/udr.yml
+if ! [ -f "$CONFIG-original" ]; then
+    mv $CONFIG $CONFIG-original
 fi
-if [ -n "$SBI_ADDR" ]; then
-    sed -z -i "s/\(udr:\n[^#]*sbi:\n *[-]\{0,1\} addr:\) [0-9.]\{1,\}/\1 $SBI_ADDR/" /etc/open5gs/udr.yaml
+if [ -z "$DB_URI" ]; then
+    DB_URI=mongodb://localhost/open5gs
 fi
-if [ -n "$SCP_ADDR" ]; then
-    sed -z -i "s/\(scp:\n[^#]*sbi:\n *[-]\{0,1\} addr:\) [0-9.]\{1,\}/\1 $SCP_ADDR/" /etc/open5gs/udr.yaml
+if [ -z "$UDR_ADDR" ]; then
+    UDR_ADDR=127.0.0.20
 fi
+if [ -z "$UDR_PORT" ]; then
+    UDR_PORT=7777
+fi
+if [ -z "$SCP_ADDR" ]; then
+    SCP_ADDR=127.0.1.10
+fi
+if [ -z "$SCP_PORT" ]; then
+    SCP_PORT=7777
+fi
+printf "db_uri: $DB_URI
+
+logger:
+    file: @localstatedir@/log/open5gs/udr.log
+
+tls:
+    enabled: no
+    server:
+      cacert: @sysconfdir@/open5gs/tls/ca.crt
+      key: @sysconfdir@/open5gs/tls/udr.key
+      cert: @sysconfdir@/open5gs/tls/udr.crt
+    client:
+      cacert: @sysconfdir@/open5gs/tls/ca.crt
+      key: @sysconfdir@/open5gs/tls/udr.key
+      cert: @sysconfdir@/open5gs/tls/udr.crt
+
+udr:
+    sbi:
+      - addr: $UDR_ADDR
+        port: $UDR_PORT
+
+scp:
+    sbi:
+      - addr: $SCP_ADDR
+        port: $SCP_PORT
+
+
+parameter:
+
+max:
+
+time:
+" > $CONFIG
 /bin/open5gs-udrd
+
+
