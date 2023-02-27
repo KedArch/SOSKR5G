060147c4fe1c226a4e80a1ab1028e125025240c2
Radosław Woźniak <radoslaw.wozniak2000@gmail.com>
2023-02-27 19:12:40 +0100

    Source code instead of PPA

--- Docker/webui/download.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-cd $(dirname $0)
-if ! [ -f Dockerfile ] || ! [ -d webui ]; then
-    rm -rf Dockerfile webui
-    git clone https://github.com/open5gs/open5gs
-    mv open5gs/{webui,docker/webui/Dockerfile} .
-    rm -rf open5gs
-fi
