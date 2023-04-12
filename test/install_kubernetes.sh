fa35086ed8648426a307ea040fb4dbeba1a18974
Radosław Woźniak <radoslaw.wozniak2000@gmail.com>
2023-04-12 10:51:25 +0200

    Disable ufw in install_kubernetes.sh

--- test/install_kubernetes.sh
+++ test/install_kubernetes.sh
@@ -4,6 +4,7 @@ echo "deb [arch=amd64 signed-by=/usr/share/keyrings/kubernetes.gpg] http://apt.k
 sudo apt update
 sudo apt install kubeadm kubelet kubectl docker.io -y
 sudo apt-mark hold kubeadm kubelet kubectl
+sudo systemctl stop kubelet containerd docker
 sudo swapoff -a
 sudo sed -i "s/ swap / s/^\(.*\)$/#\1/g" /etc/fstab
 printf "overlay\nbr_netfilter\n" | sudo tee /etc/modules-load.d/containerd.conf
@@ -13,6 +14,6 @@ printf "net.bridge.bridge-nf-call-ip6tables = 1\nnet.bridge.bridge-nf-call-iptab
 sudo sysctl --system
 sudo mkdir -p /etc/containerd/certs.d/192.168.39.1
 printf "server = \"http://192.168.39.1\"\n\n[host.\"http://192.168.39.1:5000\"]\n  capabilities = [\"pull\", \"resolve\", \"push\"]\n  skip_verify = true" | sudo tee /etc/containerd/certs.d/192.168.39.1/hosts.toml
-sudo systemctl stop kubelet containerd docker
+sudo systemctl disable --now ufw
 sudo systemctl enable --now kubelet containerd docker
 sudo kubeadm config images pull
