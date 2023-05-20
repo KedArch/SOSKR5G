e98dc7d24eeebdb8967f8e3e4b4b99417ba5ac21
Radosław Woźniak <radoslaw.wozniak2000@gmail.com>
2023-05-20 13:49:34 +0200

    add master-3 to vms.sh

--- test/vms.sh
+++ test/vms.sh
@@ -2,11 +2,13 @@
 if [ "$1" == "create" ]; then
 	./create_vm.sh master-1 108
 	./create_vm.sh master-2 109
+	./create_vm.sh master-3 110
 	./create_vm.sh worker-1 111
 	./create_vm.sh worker-2 112
 elif [ "$1" == "delete" ]; then
 	./delete_vm.sh master-1
 	./delete_vm.sh master-2
+	./delete_vm.sh master-3
 	./delete_vm.sh worker-1
 	./delete_vm.sh worker-2
 else
