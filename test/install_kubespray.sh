6f02dffb4137eee1e363646c8e2935cd3692943e
Radosław Woźniak <radoslaw.wozniak2000@gmail.com>
2023-05-21 16:00:51 +0200

    fix install_kubespray.sh

--- test/install_kubespray.sh
+++ test/install_kubespray.sh
@@ -3,9 +3,9 @@ cd $(dirname $(realpath $0))
 cd ../..
 git clone git@github.com:KedArch/SOSKR5G-kubespray.git
 VENVDIR=kubespray-venv
-KUBESPRAYDIR=kubespray
+KUBESPRAYDIR=SOSKR5G-kubespray
 ANSIBLE_VERSION=2.12
-~/.local/bin/virtualenv  --python=$(which python3) $VENVDIR
+python3 -m venv $VENVDIR
 source $VENVDIR/bin/activate
 cd $KUBESPRAYDIR
 pip install -U -r requirements-$ANSIBLE_VERSION.txt
